7570c57d98528435ac1af1ed8dc4d46f5350a9b6
Lalika31253 <Lalika31253@gmail.com>
2024-01-17 00:01:28 -0700

    Update getUserWithEmail, getUserWithId, addUser functions to use the lightbnb database with SQL queries

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -20,14 +20,27 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  // let resolvedUser = null;
+  // for (const userId in users) {
+  //   const user = users[userId];
+  //   if (user && user.email.toLowerCase() === email.toLowerCase()) {
+  //     resolvedUser = user;
+  //   }
+  // }
+  // return Promise.resolve(resolvedUser);
+  const query = 'SELECT * FROM users WHERE email =$1';
+  return pool.query(query, [email])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log("Email error is:", err);
+      throw err;
+    });
 };
 
 /**
@@ -36,7 +49,20 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  // return Promise.resolve(users[id]);
+  const query = 'SELECT * FROM users WHERE id =$1';
+  return pool.query(query, [id])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log("User id error is:", err);
+      throw err;
+    });
 };
 
 /**
@@ -45,10 +71,23 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+  const query = 'INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING *';
+  return pool.query(query, [user.name, user.email, user.password])
+  .then((result) => {
+    if(result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      return null;
+    }
+  })
+  .catch((err) => {
+    console.log("User eror is:", err);
+    throw err;
+  })
 };
 
 /// Reservations
